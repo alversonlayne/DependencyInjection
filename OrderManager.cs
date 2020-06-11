214b0b4e8c945c298680307542562ec3e977c003
Alverson Layne <alverson.layne2@gmail.com>
2020-06-10 23:01:37 -0400

    Add project files.

---
+++ OrderManager.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DependencyInjection
+{
+    public class OrderManager
+    {
+        public void Submit(Product product, string creditCardNumber, string expiryDate)
+        {
+
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace DependencyInjection
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var product = string.Empty;
+            while(product != "exit")
+            {
+                Console.WriteLine(@"Enter a product:");
+                product = Console.ReadLine();
+            }
+        }
+    }
+}
