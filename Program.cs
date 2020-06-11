553cec3db7000e3aaea9cc5798b241cbd58550c9
Alverson Layne <alverson.layne2@gmail.com>
2020-06-10 23:03:37 -0400

    Initial Commit: Dependency Injection Exercise Created public enum Product and OrderManager class

--- Program.cs
+++ Program.cs
@@ -7,9 +7,13 @@ namespace DependencyInjection
         static void Main(string[] args)
         {
             var product = string.Empty;
-            while(product != "exit")
+            while (product != "exit")
             {
-                Console.WriteLine(@"Enter a product:");
+                Console.WriteLine(@"Enter a product:
+Keyboard = 0,
+        Mouse = 1,
+        Mic = 2,
+        Speaker = 3");
                 product = Console.ReadLine();
             }
         }
